0a3dc5b3a9984f09c4784c5b8c7f295f4513e061
Alexandre Bouvier <alexandre.b2506@gmail.com>
2020-08-26 10:04:44 +0200

    Minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  after_create :fake_job
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def fake_job
+    FakeJob.perform_now
+    FakeJob.perform_later
+  end
 end
